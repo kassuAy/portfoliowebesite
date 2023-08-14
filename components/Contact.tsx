e1051fe7e2701da5ae26fe5a45ae786a96c8b343
kassuat2112 <109513483+kassuat2112@users.noreply.github.com>
2023-08-13 22:31:31 -0700

    fixed errorrs

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -93,7 +93,9 @@ const Contact = () => {
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="email"
                     />
-                  </div>
+                  </div
+                  
+                  >
                   <div className="flex flex-col py-2">
                     <label className="uppercase text-sm py-2">Subject</label>
                     <input
